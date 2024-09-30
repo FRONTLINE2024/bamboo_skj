4a0a1a564b27c7a0ff4161d8c680ff66cb51a85b
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2024-09-30 17:18:36 +0900

    chore: msw 환경 세팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,16 @@ import RecoilRootWrapper from '@/components/provider/RecoilWrapper';
 import { SocketProvider } from '@/components/provider/SocketWrapper';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { MSWComponent } from '@/components/provider/MSWComponent';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SocketProvider>
-      <RecoilRootWrapper>
-        <Component {...pageProps} />
-      </RecoilRootWrapper>
-    </SocketProvider>
+    <MSWComponent>
+      <SocketProvider>
+        <RecoilRootWrapper>
+          <Component {...pageProps} />
+        </RecoilRootWrapper>
+      </SocketProvider>
+    </MSWComponent>
   );
 }
